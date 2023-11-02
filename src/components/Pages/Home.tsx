3c5eaefe28be45ce652a786409a6dc00152432e9
leasabus <leandro.sabus@gmail.com>
2023-11-02 14:48:00 -0300

    fixed

--- src/components/Pages/Home.tsx
+++ src/components/Pages/Home.tsx
@@ -1,5 +1,3 @@
-import ReactTyped from "react-typed";
-
 export const Home = () => {
   return (
     <>
@@ -14,22 +12,7 @@ export const Home = () => {
         <h2 className="text-2xl md:text-4xl font-bold mt-4">
           Full-Stack Developer
         </h2>
-        <h4 className="text-xl text-text mt-4">
-          <ReactTyped
-            strings={[
-              "React.",
-              "NodeJS.",
-              "Typescript.",
-              "Javascript.",
-              "TailwindCSS.",
-              "SASS.",
-              "MongoDB",
-            ]}
-            typeSpeed={30}
-            backSpeed={30}
-            loop
-          />
-        </h4>
+        <h4 className="text-xl text-text mt-4"></h4>
 
         <button className="mt-6 bg-transparent  font-semibold  py-2 px-4 border border-secondary hover:bg-secondary rounded">
           <a href="#contact">Contact Me</a>
