2789b5e8c1f7e0991c4ea2d4ea37b8fddce986bd
leasabus <leandro.sabus@gmail.com>
2023-11-02 16:12:52 -0300

    modal button

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -13,34 +13,36 @@ export const Modal = ({ open, onClose, proyectData }: Props) => {
 
   return (
     <>
-      <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center overflow-y-auto animate__animated animate__fadeIn ">
+      <div
+        onClick={() => onClose()}
+        className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center overflow-y-auto animate__animated animate__fadeIn "
+      >
         <div
           className="bg-[#343a40] w-[350px] md:w-[500px] h-[400px] md:h-[450px] rounded-xl flex flex-col justify-center items-center my-auto"
           key={proyectData.id}
         >
-          <div className="flex flex-row ">
-            <button
-              onClick={() => onClose()}
-              className="btn absolute top-[13%] md:top-[15%] left-[85%] md:left-[65%]  "
-            >
-              <AiFillCloseCircle size={28} />
-            </button>
-            <h1 className=" mt-4 md:mt-8 text-2xl font-bold  text-secondary">
-              {proyectData.proyect}
-            </h1>
-          </div>
-          <span className=" mt-1 md:mt-6 px-4 md:px-12 py-4 ">
+          <button
+            onClick={() => onClose()}
+            className="absolute top-2 right-2 text-white bg-[#343a40] hover:bg-[#3e444a] p-2 rounded-full z-10"
+          >
+            <AiFillCloseCircle size={24} />
+          </button>
+          <h1 className=" mt-0 md:mt-4 text-2xl font-bold  text-secondary">
+            {proyectData.proyect}
+          </h1>
+
+          <span className=" mt-1 md:mt-2 px-4 md:px-12 py-4 md:text-xl ">
             {proyectData.description}
           </span>
           <span className="mt-4 font-bold text-secondary text-xl ">
             Tech Stack
           </span>
-          <div className="flex flex-row items-center mt-4 gap-6">
+          <div className="flex flex-row items-center mt-2 gap-6">
             {proyectData.stack?.map((stacks, index) => (
               <RenderStack key={index} stack={stacks} />
             ))}
           </div>
-          <div className="flex flex-row items-center gap-4 mt-1 md:mt-6">
+          <div className="flex flex-row items-center gap-4 mt-1 md:mt-2">
             <button
               onClick={() => onClose()}
               className="mt-4 md:mt-6 bg-transparent  font-semibold  py-2 px-4 border border-secondary hover:bg-secondary rounded"
@@ -62,12 +64,6 @@ export const Modal = ({ open, onClose, proyectData }: Props) => {
               </a>
             </button>
           </div>
-          {/* <button
-            onClick={() => onClose()}
-            className="btn absolute top-[13%] md:top-[15%] left-[85%] md:left-[65%] "
-          >
-            <AiFillCloseCircle size={28} />
-          </button> */}
         </div>
       </div>
     </>
